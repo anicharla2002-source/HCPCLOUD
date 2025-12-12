2f4c3ccffb000e16a6c3f30ea83acb30202d95d6
anicharla2002-source <anicharla2002@gmail.com>
2025-12-12 17:06:55 +0530

    Modify AWS instance configuration in main.tf
    Updated key_name and security group ID for AWS instance.

--- main.tf
+++ main.tf
@@ -6,8 +6,8 @@ resource "aws_instance" "one" {
   count                  = 4
   ami                    = "ami-0ecb62995f68bb549"
   instance_type          = "c7i-flex.large"
-  key_name               = "rahamdocker"
-  vpc_security_group_ids = ["sg-058a8c775a14e219a"]
+  key_name               = "default"
+  vpc_security_group_ids = ["sg-09a6880fc8b6299d6"]
   tags = {
     Name = var.instance_names[count.index]
   }
@@ -16,4 +16,5 @@ resource "aws_instance" "one" {
 variable "instance_names" {
   default = ["jenkins", "tomcat-1", "tomcat-2", "Monitoring server"]
 }
+
 
